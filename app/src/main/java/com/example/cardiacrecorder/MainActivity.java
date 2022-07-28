a18568f12116ccd73f2fb2081f26808c58c0b4c3
tanni <saha1807101@stud.kuet.ac.bd>
2022-07-28 19:00:39 +0600

    splash added

--- app/src/main/java/com/example/cardiacrecorder/MainActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Home.class));
                 finish();
             }
-
         };
 
         Handler h = new Handler();
